e5afaa04136ef0ce5359c0c7e200d3b4a6234e5e
kven0101 <kven0101@yahoo.com.tw>
2021-05-03 16:11:10 +0800

    add rest complete, fulfill n-n relationship in DB

--- be/controller/addrest_controller.js
+++ be/controller/addrest_controller.js
@@ -1,69 +1,66 @@
 // onst addfavrest = require('../routes/login');
 
-const Favrest = require('../models/restaurant');
+const Restaurant = require('../models/restaurant');
 
-const Favuser = require('../models/user');
+const User = require('../models/user');
 
-exports.addfavrest = function (req, res) {
+exports.addfavrest = (req, res) => {
   const placename = req.body.enterplacename;
   const favusername = req.user.name;
   const { placeId, geometry, name } = req.body;
-  console.log('==========================');
-  console.log('呼叫Controller成功，資訊如下:');
-  console.log(placename);
-  console.log(placeId);
-  console.log(geometry);
-  console.log(name);
-  console.log('==========================');
-  console.log('目前使用者是:');
-  console.log(favusername);
 
-  // 要抓其他的資訊
-  // const placeid = req.body.place_id;
-  // const placegeo = req.body.geometry;
-  // const placeaddress = req.body.place.formatted_address;
-  // console.log(placeid);
-  // console.log(placegeo);
-  // console.log(placeaddress);
-  // eslint-disable-next-line consistent-return
-
-  const newfavrest = new Favrest();
-  newfavrest.formatted_address = placename;
-  newfavrest.place_id = placeId;
-  newfavrest.geometry = geometry;
-  newfavrest.name = name;
-  newfavrest.favuser = favusername;
-  newfavrest.save((err2) => {
-    if (err2) {
-      throw err2;
+  Restaurant.findOne({ place_id: placeId }, (err, restexist) => {
+    if (err) return (err);
+    if (restexist) {
+      Restaurant.findOneAndUpdate(
+        {
+          place_id: placeId,
+        },
+        {
+          $addToSet: {
+            favuser: favusername,
+          },
+        },
+        (docerr, doc) => {
+          if (docerr) {
+            console.log('xXXXXXXXXXXXXXXXXXX');
+          }
+          console.log(doc);
+        },
+      );
+    } else {
+    // if no info, create new rest
+      const newfavrest = new Restaurant();
+      newfavrest.formatted_address = placename;
+      newfavrest.place_id = placeId;
+      newfavrest.geometry = geometry;
+      newfavrest.name = name;
+      newfavrest.favuser = favusername;
+      newfavrest.save((err2) => {
+        if (err2) {
+          throw err2;
+        }
+        // success, return the new rest
+        return (newfavrest);
+      });
     }
-    // success, return the new rest
-    return (newfavrest);
   });
 
-  let newfavuser = mongoose.model("User", UserSchema);
-  
-  const newfavuser = new Favuser();
-
-  newfavuser.findOneAndUpdate(
+  User.findOneAndUpdate(
     {
-      favrest: '',
+      name: favusername,
     },
     {
-      favrest: name,
+      $addToSet: {
+        favrest: name,
+      },
     },
-    {
-      upsert: true,
+    (docerr, doc) => {
+      if (docerr) {
+        console.log('xXXXXXXXXXXXXXXXXXX');
+      }
+      console.log(doc);
     },
   );
-
-  // newfavuser.save((err2) => {
-  //   if (err2) {
-  //     throw err2;
-  //   }
-  //   // success, return the new rest
-  //   return (newfavrest);
-  // });
-
   res.redirect('/addrestaurant');
 };
